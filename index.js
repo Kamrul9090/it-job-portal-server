66f82cc68dca164d6beb7428ae9e08c5c3feed61
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2023-02-18 11:29:12 +0600

    added fresherJob collection API

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express()
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const fresherJobCollection = client.db('ItJobPortal').collection('fresherJob');
+
+        app.get('/fresherJob', async (req, res) => {
+            const query = {};
+            const result = await fresherJobCollection.find(query).toArray();
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(e => console.error(e))
+
+
+
 
 app.get('/', (req, res) => {
     res.send('server open on port 5000');
